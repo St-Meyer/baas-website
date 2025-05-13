d69f8b4bcfffd5de18b7d6898580066bf98f930c
user <user@debian.debian>
2025-05-13 10:33:44 +0200

    repaired Github Login

--- _javascript/login.js
+++ _javascript/login.js
@@ -1,11 +1,12 @@
 function getLoginToken() {
-	$.get("http://localhost:4848/user/login/github", (data, status, xhr) => {
-		if (status != "success") {
-			console.error("Failed to get the login token")
-		}
-		
-		console.log(status)
-		let uri = data.slice(25)
-		window.location.href = uri
-	})
+	 //$.get("http://localhost:4848/user/login/github", (data, status, xhr) => {
+	//	if (status != "success") {
+	//		console.error("Failed to get the login token")
+	//		return
+	//	}
+
+	//	console.log("Redirecting to Github OAuth:", data)
+	//	window.location.href = data.trim()
+	//})
+	window.location.href = "http://localhost:4848/user/login/github";
 }
